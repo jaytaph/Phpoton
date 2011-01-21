743d9515bac0052937bc389453a333f31c62887a
root <root@debian-jth.enrise.com>
2011-01-21 21:07:01 +0100

    Initial twitter connection implemented

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,27 @@
 <?php
 
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
-{
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
 
+    function __construct($application) {
+        parent::__construct($application);
 
+        // Set timezone
+        date_default_timezone_set("Europe/Amsterdam");
+
+        // Load config into register
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV);
+        Zend_Registry::set('config', $config);
+
+        // Initialize and save twitter object
+        $accessToken = new Zend_Oauth_Token_Access();
+        $accessToken
+            ->setToken($config->settings->twitter->accessToken)
+            ->setTokenSecret($config->settings->twitter->accessTokenSecret);
+        $data = array('username' => $config->settings->twitter->screenName,
+                      'accessToken' => $accessToken);
+
+        $twitter = new Zend_Service_Twitter($data);
+        Zend_Registry::set('twitter', $twitter);
+    }
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,17 +5,71 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+//        print "<pre>";
+//        session_start();
+//        var_dump ($_SESSION);
     }
 
-    public function indexAction()
-    {
-        // action body
+//    public function authenticateAction()
+//    {
+//        $config = Zend_Registry::get('config');
+//        $oauth = new Zend_Oauth_Consumer($config->settings->oauth->config);
+//        $requestToken = $oauth->getRequestToken();
+////        print "<pre>";
+////        var_dump ($requestToken);
+//
+//        $_SESSION['reqtoken'] = serialize($requestToken);
+//        session_write_close();
+//
+//        print "<a href=".$oauth->getRedirectUrl().">Click here</a>";
+//        exit;
+//    }
+
+//    public function callbackAction()
+//    {
+//        $config = Zend_Registry::get('config');
+//        $oauth = new Zend_Oauth_Consumer($config->settings->oauth->config);
+////        $requestToken = $oauth->getRequestToken();
+////        print "<pre>";
+////        var_dump ($requestToken);
+//
+////        $_SESSION['reqtoken'] = serialize($requestToken);
+////        session_write_close();
+////        exit;
+//
+//        $requestToken = unserialize($_SESSION['reqtoken']);
+//        $accessRequestToken = $oauth->getAccessToken($_GET, $requestToken);
+//        var_dump ($accessRequestToken);
+//        $_SESSION['acc_req_token'] = serialize($accessRequestToken);
+//        sesssion_write_close();
+//        exit;
+//    }
+
+    public function indexAction() {
+        $twitter = Zend_Registry::get('twitter');
+        //$response = $twitter->status->friendsTimeline();
+        $response = $twitter->directMessage->messages();
+
+        print "<table>";
+        print "<tr><th>Thumb</th><th>User</th><th>Tweet</th></tr>";
+        foreach ($response->direct_message as $status) {
+            print "<tr>";
+            print "<td><img src='".$status->sender->profile_image_url."'></td>";
+            print "<td>".$status->sender->screen_name."</td>";
+            print "<td>".$status->text."</td>";
+            print "</tr>";
+        }
+        print "</table>";
+
+        exit;
     }
 
-    public function tweetsAction()
+    public function tweetAction()
     {
-        // action body
+        $twitter = Zend_Registry::get('twitter');
+
+        $twitter->status->update("Bliep! Hello world, again!");
+        exit;
     }
 
 
