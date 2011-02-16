9d890102959c80d0a98ccf56c26f162ea2754ec3
Tim de Pater <git@trafex.nl>
2011-02-16 09:49:58 +0100

    Added patch for the new blocklist table

---
+++ data/sql/patch-002.sql
@@ -0,0 +1,9 @@
+--
+-- Table structure for table `blocklist`
+--
+
+CREATE TABLE IF NOT EXISTS `blocklist` (
+  `id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
