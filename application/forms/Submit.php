18f69c6784a0ef4103d141da6f27e3e1cd71062b
Joshua Thijssen <jthijssen@noxlogic.nl>
2011-02-27 21:20:17 +0100

    Added userdefined form to submit

--- application/forms/Submit.php
+++ application/forms/Submit.php
@@ -8,36 +8,37 @@ class Form_Submit extends Zend_Form {
 
         $this->setName('submit_a_question');
 
-//        $twitter = new Zend_Form_Element_Text('twitter');
-//        $twitter->setLabel('Your twitter name')
-//                ->setAttrib('maxlength', 35)
-//                ->addValidator('NotEmpty')
-//                ->addFilter('StripTags');
-
         $fullname = new Zend_Form_Element_Text('fullname');
-        $fullname->setLabel('Your full name')
-                ->setRequired(true)
-                ->setAttrib('maxlength', 50)
+        $fullname->setRequired(true)
+                 ->setDecorators(array('ViewHelper','Errors'))
+                 ->setAttrib('maxlength', 50)
+                 ->addFilter('StripTags');
+        
+        $twitter = new Zend_Form_Element_Text('twitter');
+        $twitter->setAttrib('maxlength', 35)
+                ->setDecorators(array('ViewHelper','Errors'))
+                ->addValidator('NotEmpty')
                 ->addFilter('StripTags');
 
         $question = new Zend_Form_Element_Textarea('question');
-        $question->setLabel('Question')
-                ->setRequired(true)
-                ->addValidator('NotEmpty')
-                ->setAttrib('cols', 50)
-                ->setAttrib('rows', 3)
-                ->setAttrib('onKeyDown','limitText(this.form.question,this.form.countdown,'.self::MAX_QUESTION_LENGTH.');')
-                ->setAttrib('onKeyUp','limitText(this.form.question,this.form.countdown,'.self::MAX_QUESTION_LENGTH.');');
+        $question->setRequired(true)
+                 ->setDecorators(array('ViewHelper','Errors'))
+                 ->addValidator('NotEmpty')
+                 ->setAttrib('cols', 50)
+                 ->setAttrib('rows', 3)
+                 ->setAttrib('onKeyDown','limitText(this.form.question,this.form.countdown,'.self::MAX_QUESTION_LENGTH.');')
+                 ->setAttrib('onKeyUp','limitText(this.form.question,this.form.countdown,'.self::MAX_QUESTION_LENGTH.');');
 
         $countdown = new Zend_Form_Element_Text('countdown');
         $countdown->setAttrib('readonly', 'readonly')
-                ->setAttrib('size', 3)
-                ->setValue(self::MAX_QUESTION_LENGTH)
-                ->setDescription('chars left');
+                  ->setDecorators(array('ViewHelper','Errors'))
+                  ->setAttrib('size', 3)
+                  ->setValue(self::MAX_QUESTION_LENGTH)
+                  ->setDescription('chars left');
 
         $answer = new Zend_Form_Element_Text('answer');
-        $answer->setLabel('Answer')
-               ->setRequired(true)
+        $answer->setRequired(true)
+               ->setDecorators(array('ViewHelper','Errors'))
                ->addValidator('NotEmpty')
                ->addFilter('StripTags')
                ->setAttrib('maxlength', 50)
@@ -51,12 +52,14 @@ class Form_Submit extends Zend_Form {
             'imgDir' => APPLICATION_PATH . "/../public/images/captcha"));
 
         $captcha = new Zend_Form_Element_Captcha('captcha', $options);
-        $captcha->setLabel('I do not tolerate other bots. Proof you are human!');
 
         $submit = new Zend_Form_Element_Submit('submit');
-        $submit->setLabel('Submit question to phpoton');
+        $submit->setLabel('Submit question to phpoton')
+               ->setDecorators(array('ViewHelper','Errors'));
 
-        $this->addElements(array($fullname, $question, $countdown, $answer, $captcha, $submit));
+        $this->addElements(array($fullname, $twitter, $question, $countdown, $answer, $captcha, $submit));
+
+        $this->setDecorators(array(array('ViewScript', array('viewScript' => 'submit/submitform.phtml'))));
     }
 }
  
